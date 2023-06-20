d80795ee1e544cabe07f8056b2abc9ea3b92c514
vinod <anantavinodkumar@gmail.com>
2023-06-20 13:46:24 +0530

    first-commit

---
+++ src/components/PageTitle.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from '../styles/modules/title.module.scss';
+
+const PageTitle = ({children, ...rest}) => {
+    return (
+        <p className={styles.title}>{children}</p>
+    )
+}
+
+export default PageTitle;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '@fontsource/poppins';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import './styles/GlobalStyles.css';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
